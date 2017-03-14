aa3dbfc1a50172964c747d82b6288086c0364ee8
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-03-14 20:19:41 +0100

    electron test-runner

---
+++ index.js
@@ -0,0 +1,49 @@
+
+(async function() {
+  let remote = require('electron').remote;
+  let argv = remote.process.argv;
+  let filename = argv[argv.length - 1];
+  if(filename[0] !== '/') {
+    filename = remote.process.cwd() + '/' + filename;
+  }
+  console.log('loading file: ' + filename);
+  let module = require(filename);
+
+  let visited = new Map();
+  let tests = [];
+
+  traverse(module);
+  function traverse(o) {
+    if(visited.get(o)) {
+      return;
+    }
+    visited.set(o, true);
+
+    var keys = Object.getOwnPropertyNames(o);
+    console.log(keys.join(' '));
+    keys.forEach(key => {
+      traverse(o[key]);
+      if(key.startsWith('TEST_')) {
+        tests.push({
+          name: key,
+          fn: o[key]
+        });
+      }
+    });
+  }
+
+  console.log(tests.map(o => o.name).join(' '));
+  let ok = true;
+  for(let i = 0; i < tests.length; ++i) {
+    let test = tests[i];
+    console.log('Running ' + test.name);
+    try {
+      await Promise.resolve(test.fn);
+      console.log(test.name + ' ok.');
+    } catch (e) {
+      console.log(e);
+      console.log(test.name + ' error: ' + e.toString());
+    }
+    remote.process.exit(ok ? 0 : 1);
+  }
+})();
---
+++ main.js
@@ -0,0 +1,21 @@
+const {app, BrowserWindow} = require('electron')
+const path  = require('path');
+const url = require('url');
+
+let w;
+app.on('ready', function() {
+    w = new BrowserWindow({
+//      'show': false,
+      'webPreferences': {'webSecurity': false}
+    });
+
+    w.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    w.webContents.openDevTools();
+    w.on('closed', () => w = null);
+});
+app.on('window-all-closed', () => app.quit());
